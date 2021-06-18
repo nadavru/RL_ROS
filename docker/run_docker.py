6c8526b1b0a6dcea897d4d253ad743830d6738f2
orrlani <olaniado@campus.technion.ac.il>
2021-06-18 22:43:00 +0300

    adding plot ading docker run file

---
+++ docker/run_docker.py
@@ -0,0 +1,90 @@
+import subprocess
+import time
+from multiprocessing import Pool
+
+IMAGE_NAME = "new_11"
+from multiprocessing import Pool
+
+
+def iteration(index):
+    print(index)
+    # temporary - need to change by the interactions
+    name_of_container = "rl_learning_{0}".format(index)
+
+    command_init = "docker run -i --name {0} {1}".format(name_of_container, name_of_container)
+    container_init = subprocess.Popen([command_init],
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE, shell=True)
+
+    stdout, stderr = container_init.communicate()
+
+    # convert stderr to string
+    stderr_str = stderr.decode("utf-8")
+
+    # image that continue the container not exist create container from the base image
+
+    if "Unable" not in stderr_str.split(" "):
+        print("container {0} continue from last commit".format(name_of_container))
+
+    else:
+        command_init = "docker run -i --name {0} {1}".format(name_of_container, IMAGE_NAME)
+        container_init = subprocess.Popen([command_init],
+                                          stdout=subprocess.PIPE,
+                                          stderr=subprocess.PIPE, shell=True)
+        print("container {0} start firat time".format(name_of_container))
+
+    # assume container continue to work
+    command_start = "docker start {0}".format(name_of_container)
+    container_start = subprocess.Popen([command_start],
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE, shell=True)
+
+    command_run_simulation = "docker exec -i {0} bash ./root/catkin_ws/simulator.sh".format(name_of_container)
+    container_run_simulation = subprocess.Popen([command_run_simulation],
+                                                stdout=subprocess.PIPE,
+                                                stderr=subprocess.PIPE, shell=True)
+
+    command_run_agent = "docker exec -i {0} bash ./root/catkin_ws/agent.sh {1}".format(name_of_container, index)
+    container_run_agent = subprocess.Popen([command_run_agent],
+                                           stdout=subprocess.PIPE,
+                                           stderr=subprocess.PIPE, shell=True)
+    time.sleep(200)
+    command_pass = "docker cp {0}:/root/catkin_ws/src/deep_learning/scripts/model_{1}/ /home/makers/rl_docker/codeRos".format(
+        name_of_container, index)
+    container_pass = subprocess.Popen([command_pass],
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE, shell=True)
+
+    time.sleep(10)
+    # DOCKER STOP - to stop docker from run
+
+    command_commit = "docker commit {0} {0}".format(name_of_container)
+
+    container_commit = subprocess.Popen([command_commit],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE, shell=True)
+    time.sleep(10)
+    command_stop = "docker stop {0}".format(name_of_container)
+    container_stop = subprocess.Popen([command_stop],
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE, shell=True)
+
+    print("container {0} finished!!".format(name_of_container))
+
+
+def main():
+    while 1:
+        array = [i for i in range(1, 15)]
+        p = Pool()
+        p.map(iteration, array)
+
+
+if __name__ == '__main__':
+    iteration(10)
+    iteration(10)
+    # main()
+    # command = "\"/home/docker/catkin_ws/src/Indoors_main/node_manager/scripts/indoors_inside_docker.py " + str(
+    #     container_num) + " " + \
+    #           str(script_name) + " " + ROS_MASTER_URI + " " + ROS_IP + " " + batch_name + "\""
+    #
+    # super_cmd = "docker exec -t indoors_container_" + str(container_num) + " sh -c " + command
--- part1/plot.py
+++ part1/plot.py
@@ -11,6 +11,74 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+
+
+
+
+def _add_data(places_data:list,info_data:list,postion:int,color,axis,axis_num:int,axis_name:str):
+    """
+    info_data: distance,loss,loss_e,loss_p,loss_v
+    """
+
+    position_episodes = []
+    for place in range(len(places_data)):
+        if places_data[place] == postion:
+            position_episodes.append(place)
+
+    # The score in the same episode
+    position_info = []
+    for episode in position_episodes:
+        position_info.append(info_data[episode])
+
+
+    # set title of grath
+    # axis[axis_num].title(f"{axis_name} graph")
+    # set x lable
+    axis[axis_num].set_xlabel("episode")
+    # set y lable
+    axis[axis_num].set_ylabel(axis_name)
+
+
+    axis[axis_num].scatter(position_episodes, position_info, color=color, label="position {}".format(postion))
+
+
+def _get_color_grath_name(place_index,data_index)->(str,str):
+    place_color =""
+    grath_name =""
+
+    if place_index == 0:
+        place_color ="red"
+    if place_index == 1:
+        place_color = "blue"
+    if place_index == 2:
+        place_color = "green"
+    if place_index == 3:
+        place_color = "black"
+    if place_index == 4:
+        place_color = "yellow"
+    if place_index == 5:
+        place_color = "white"
+
+    if data_index == 0:
+        grath_name = "reward"
+    if data_index == 1:
+        grath_name = "distance"
+
+    if data_index == 2:
+        grath_name = "loss"
+
+    if data_index == 3:
+        grath_name = "loss_e"
+
+    if data_index == 4:
+        grath_name = "loss_p"
+
+    if data_index == 5:
+        grath_name = "loss_v"
+
+    return place_color,grath_name
+
+
 def plot(folder=None):
 
     if folder is None:
@@ -28,34 +96,68 @@ def plot(folder=None):
                 places = np.concatenate(places, axis=None)'''
     
     # places = np.random.randint(1,21, size=(275,))
-    places = np.random.randint(1, 3, size=(275,))
+
 
     # open files
-    for name in ["reward"]:#, "distance", "loss", "loss_e", "loss_p", "loss_v"]:
-        data = []
-        if not os.path.exists(f"{folder}{name}"):
-            continue
+    data_list =[]
+    for name in ["reward", "distance","loss","loss_e","loss_p","loss_v"]:#, "distance", "loss", "loss_e", "loss_p", "loss_v"]:
+        # if not os.path.exists(f"{folder}{name}"):
+        #     continue
         with open(f"{folder}{name}", 'rb') as f:
-            try:
-                while 1:
-                    data.append(np.load(f))
-            except:
-                data = np.concatenate(data, axis=None)
+            data_list.append(np.load(f))
 
-        episodes = np.arange(data.shape[0])
+    episodes = np.arange(data_list[0].shape[0])
 
+    places = np.random.randint(0, 6, size=len(episodes))
 
+    PLACES_NUM = 6
+    fig, axis = plt.subplots(PLACES_NUM) # for every data
 
-        # set title of grath
-        plt.title(f"{name} graph")
-        # set x lable
-        plt.xlabel("episode")
-        # set y lable
-        plt.ylabel("reward")
-        # set grath
-        plt.scatter(episodes, data, color ="red")
+    # _add_data(places_data=places, info_data=data_list[0], postion=0, color="red", axis=axis,
+    #           axis_num=0,
+    #           axis_name="yes")
+    #
+    # _add_data(places_data=places, info_data=data_list[0], postion=0, color="red", axis=axis,
+    #           axis_num=0,
+    #           axis_name="yes")
+    for data_index in range(len(data_list)):
+        for place_index in range(0,PLACES_NUM):
+            color ,grath_name = _get_color_grath_name(place_index,data_index)
+            _add_data(places_data=places,info_data=data_list[data_index], postion=place_index,color=color,axis= axis,
+                          axis_num=data_index,
+                axis_name=grath_name)
+    labels = []
+    for ax in fig.axes:
+        _, axLabel = ax.get_legend_handles_labels()
+        labels.extend(axLabel)
+
+    labels = list(dict.fromkeys(labels))
+    fig.legend(labels,
+                   loc='upper right')
+        # All episodes where position 2 is selected
+        # position_2_episodes = []
+        # for place in range(len(places)):
+        #     if places[place] == 2:
+        #         position_2_episodes.append(place)
+        #
+        # data_reward = data
+        # # The score in the same episode
+        # position_2_reward = []
+        # for episode in position_2_episodes:
+        #     position_2_reward.append(data_reward[episode])
+
+        # # set title of grath
+        # plt.title(f"{name} graph")
+        # # set x lable
+        # plt.xlabel("episode")
+        # # set y lable
+        # plt.ylabel("reward")
+        # # set grath
+
+        # plt.scatter(position_2_episodes, position_2_reward, color="blue",label = "position 2")
         # show grath
-        plt.show()
+        #plt.legend(loc="upper left")
+    plt.show()
 
 
 def test():
@@ -73,4 +175,7 @@ def test():
     plt.show()
 if __name__=="__main__":
     #plot("model_aac")
-    plot("/home/makers/rl_docker/codeRos/model_1")
+
+    dir = os.getcwd() + '/' +'data'
+    print(os.path.isdir(dir))
+    plot(dir)
