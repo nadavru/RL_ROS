e3d5a98f6102f166ee5dfe0e7bb529b7f5b30ce6
orrLani <54217343+orrLani@users.noreply.github.com>
2021-06-26 01:11:26 +0300

    Add files via upload
    update

--- docker/run_docker.py
+++ docker/run_docker.py
@@ -1,23 +1,27 @@
 import subprocess
 import time
+import os
 from multiprocessing import Pool
 
 IMAGE_NAME = "orr_nadav_rl_base"
-from multiprocessing import Pool
-import os
-TIME = 5 * 60 # (5 min)
-import time
+TIME = 7 * 60  # (7 min)
+
+
 def iteration(index):
+
     print(index)
     # temporary - need to change by the interactions
     name_of_container = "orr_nadav_rl_{0}".format(index)
 
-    #command_init = "docker run -i --name {0} {1}".format(name_of_container, name_of_container)
+    # command_init = "docker run -i --name {0} {1}".format(name_of_container, name_of_container)
 
     # command_init = "docker run -i --name {0} {1}".format(name_of_container, name_of_container)
     # docker run - d - -name orr_nadav orr_nadav_rl_base sleep 10; sleep 10; docker rm - f orr_nadav
-    command_init ="docker run -i --name {0} {1} sleep {2}; sleep 10; docker rm - f {3}".format(name_of_container,name_of_container,
-                                                                                                 TIME,name_of_container)
+
+    # If container run from second time
+    command_init = "docker run -i --name {0} {1} sleep {2}; sleep 10; docker rm - f {3}".format(name_of_container,
+                                                                                                name_of_container, TIME,
+                                                                                                name_of_container)
     container_init = subprocess.Popen([command_init],
                                       stdout=subprocess.PIPE,
                                       stderr=subprocess.PIPE, shell=True)
@@ -27,7 +31,7 @@ def iteration(index):
     # convert stderr to string
     stderr_str = stderr.decode("utf-8")
 
-    # image that continue the container not exist create container from the base image
+    # create container from the base image (image that continue the container not exist)
 
     if "No" not in stderr_str.split(" "):
         print("container {0} continue from last commit".format(name_of_container))
@@ -45,7 +49,25 @@ def iteration(index):
     container_start = subprocess.Popen([command_start],
                                        stdout=subprocess.PIPE,
                                        stderr=subprocess.PIPE, shell=True)
-    time.sleep(5)
+
+    my_location = os.path.dirname(os.path.abspath(__file__))
+    # move files from global to local container
+    # c_p ="cp `ls - t model_2/* | head - 10` ./h"
+    # comm_pass = "cp `ls - t {0}/model_{1}/* | head - 10` ./".format(my_location,index)
+
+    # comm_pass_1 = "docker cp `ls - t {0}/model_{1}/* | head - 10` ./".format(my_location, index)
+
+    # command_pass = "docker cp `ls - t {0}/model_{1}/* | head - 10` {2}:
+    # /root/catkin_ws/src/deep_learning/scripts/part1/model_{2}/".format(my_location,
+    #    index,name_of_container,index)
+
+    print("move files from local to container")
+    command_pass = "docker cp {0}/RL_ROS/model_{1} {2}:/root/catkin_ws/src/deep_learning/scripts/part1/".format(
+        my_location, index, name_of_container, index)
+
+    global_pass = subprocess.Popen([command_pass], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+    time.sleep(10)
     command_run_simulation = "docker exec -i {0} bash ./root/catkin_ws/simulator.sh".format(name_of_container)
     container_run_simulation = subprocess.Popen([command_run_simulation],
                                                 stdout=subprocess.PIPE,
@@ -56,25 +78,21 @@ def iteration(index):
                                            stdout=subprocess.PIPE,
                                            stderr=subprocess.PIPE, shell=True)
     time.sleep(5)
-    # pass
 
-    timeout = time.time() + 60*5   # 5 minutes from now
-    my_location = os.path.dirname(os.path.abspath(__file__))
+    # pass files
+    timeout = time.time() + TIME
     pass_index = 0
+    print("move files from container to local")
     while True:
         print("is time to pass {0} in {1}".format(pass_index,name_of_container))
-        command_pass = "docker cp {0}:/root/catkin_ws/src/deep_learning/scripts/part1/model_{1}/ {2}/RL_ROS".format(
-        name_of_container, index,my_location)
-        container_pass = subprocess.Popen([command_pass],
-                                      stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE, shell=True)
-        if time.time() > timeout or pass_index >50 :
+        command_pass = "docker cp {0}:/root/catkin_ws/src/deep_learning/scripts/part1/model_{1}/ {2}/RL_ROS/".format(
+            name_of_container, index, my_location)
+        container_pass = subprocess.Popen([command_pass], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+        if time.time() > timeout or pass_index > 100:
             break
         pass_index += 1
-        time.sleep(60)
-
-
-
+        time.sleep(TIME//10)
 
 
 def main():
