cc8b7669e50d0a4e9d667b4960ecf63e23ff73ea
orrLani <54217343+orrLani@users.noreply.github.com>
2021-06-28 02:25:40 +0300

    Update run_docker.py

--- docker/run_docker.py
+++ docker/run_docker.py
@@ -1,10 +1,26 @@
 import subprocess
 import time
 import os
+import signal
 from multiprocessing import Pool
+import errno
+from contextlib import contextmanager
+IMAGE_NAME = "orr_nadav_rl_base_2"
+TIME = 3 * 60  # (3 min)
+from functools import wraps
 
-IMAGE_NAME = "orr_nadav_rl_base"
-TIME = 7 * 60  # (7 min)
+
+class timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
 
 
 def iteration(index):
@@ -13,60 +29,23 @@ def iteration(index):
     # temporary - need to change by the interactions
     name_of_container = "orr_nadav_rl_{0}".format(index)
 
-    # command_init = "docker run -i --name {0} {1}".format(name_of_container, name_of_container)
+    # get correct time
+    my_location = os.path.dirname(os.path.abspath(__file__))
 
-    # command_init = "docker run -i --name {0} {1}".format(name_of_container, name_of_container)
-    # docker run - d - -name orr_nadav orr_nadav_rl_base sleep 10; sleep 10; docker rm - f orr_nadav
-
-    # If container run from second time
-    command_init = "docker run -i --name {0} {1} sleep {2}; sleep 10; docker rm - f {3}".format(name_of_container,
-                                                                                                name_of_container, TIME,
-                                                                                                name_of_container)
+    # stop
+    command_stop = "docker rm --force {0}".format(name_of_container)
+    container_stop = subprocess.Popen([command_stop],
+                                         stdout=subprocess.PIPE,
+                                         stderr=subprocess.PIPE, shell=True)
+    time.sleep(10)
+    command_init = "docker run -v {0}/share_folder/catkin_ws/:/root/catkin_ws -i --name {1} {2} sleep {3}; sleep 10; docker rm - f {4}".\
+        format(my_location,name_of_container,IMAGE_NAME,TIME,name_of_container)
     container_init = subprocess.Popen([command_init],
-                                      stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE, shell=True)
-
-    stdout, stderr = container_init.communicate()
-
-    # convert stderr to string
-    stderr_str = stderr.decode("utf-8")
-
-    # create container from the base image (image that continue the container not exist)
-
-    if "No" not in stderr_str.split(" "):
-        print("container {0} continue from last commit".format(name_of_container))
-
-    else:
-        print("this is first time running {0}".format(name_of_container))
-        command_init = "docker run -i --name {0} {1} sleep {2}; sleep 10; docker rm - f {3}".format(name_of_container,IMAGE_NAME,TIME,name_of_container)
-        container_init = subprocess.Popen([command_init],
                                           stdout=subprocess.PIPE,
                                           stderr=subprocess.PIPE, shell=True)
-        print("container {0} start first time".format(name_of_container))
 
-    # assume container continue to work
-    command_start = "docker start {0}".format(name_of_container)
-    container_start = subprocess.Popen([command_start],
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE, shell=True)
 
-    my_location = os.path.dirname(os.path.abspath(__file__))
-    # move files from global to local container
-    # c_p ="cp `ls - t model_2/* | head - 10` ./h"
-    # comm_pass = "cp `ls - t {0}/model_{1}/* | head - 10` ./".format(my_location,index)
-
-    # comm_pass_1 = "docker cp `ls - t {0}/model_{1}/* | head - 10` ./".format(my_location, index)
-
-    # command_pass = "docker cp `ls - t {0}/model_{1}/* | head - 10` {2}:
-    # /root/catkin_ws/src/deep_learning/scripts/part1/model_{2}/".format(my_location,
-    #    index,name_of_container,index)
-
-    print("move files from local to container")
-    command_pass = "docker cp {0}/RL_ROS/model_{1} {2}:/root/catkin_ws/src/deep_learning/scripts/part1/".format(
-        my_location, index, name_of_container, index)
-
-    global_pass = subprocess.Popen([command_pass], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-
+    print("succee to run the container {0}".format(name_of_container))
     time.sleep(10)
     command_run_simulation = "docker exec -i {0} bash ./root/catkin_ws/simulator.sh".format(name_of_container)
     container_run_simulation = subprocess.Popen([command_run_simulation],
@@ -78,30 +57,34 @@ def iteration(index):
                                            stdout=subprocess.PIPE,
                                            stderr=subprocess.PIPE, shell=True)
     time.sleep(5)
+    # complete the action
+    time.sleep(TIME)
 
-    # pass files
-    timeout = time.time() + TIME
-    pass_index = 0
-    print("move files from container to local")
-    while True:
-        print("is time to pass {0} in {1}".format(pass_index,name_of_container))
-        command_pass = "docker cp {0}:/root/catkin_ws/src/deep_learning/scripts/part1/model_{1}/ {2}/RL_ROS/".format(
-            name_of_container, index, my_location)
-        container_pass = subprocess.Popen([command_pass], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-
-        if time.time() > timeout or pass_index > 100:
-            break
-        pass_index += 1
-        time.sleep(TIME//10)
+    # stop
+    container_stop = subprocess.Popen([command_stop],
+                                         stdout=subprocess.PIPE,
+                                         stderr=subprocess.PIPE, shell=True)
 
 
 def main():
-    array = [i for i in range(1, 31)]
+    array = [i for i in range(1,4)]
+    p = Pool()
+    p.map(iteration, array)
+
+    array = [i for i in range(1, 4)]
+    p = Pool()
+    p.map(iteration, array)
+
+    array = [i for i in range(1, 4)]
+    p = Pool()
+    p.map(iteration, array)
+
+    array = [i for i in range(1, 4)]
     p = Pool()
     p.map(iteration, array)
 
 
 if __name__ == '__main__':
-    # main()
-    iteration(2)
+    main()
+    # iteration(2)
 
